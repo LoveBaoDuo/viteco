65dd16e65a70ec9b3eabef0149ae1cfa6c953b81
bayunbuzi <2448273134@qq.com>
2022-03-05 16:08:57 +0800

    配置完成

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/route'
 import { createPinia } from 'pinia'
+// 重置样式
+import '@/style/normalize.css'
 createApp(App).use(router).use(createPinia()).mount('#app')
